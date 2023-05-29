a1e5102ed78290c160308225db6780abfb913333
DavidAshburn <oneinchwebbing@gmail.com>
2023-05-29 09:27:41 -0700

    mocked model

---
+++ charmodel.js
@@ -0,0 +1,73 @@
+class PlayerCharacter {
+
+    constructor() {
+        this.player_name = null;
+        this.character_name = null;
+        this.race = null;
+        this.racial_traits = [];
+        this.class = null;
+        this.class_features = [];
+        this.background = null;
+
+        this.trait = null;
+        this.bond = null;
+        this.ideal = null;
+        this.flaw = null;
+        this.alignment = null;
+
+        this.str = null;
+        this.dex = null;
+        this.con = null;
+        this.int = null;
+        this.wis = null;
+        this.cha = null;
+        this.saving_throws = [];
+
+        this.proficiency_bonus = null;
+        this.passive_perception = null;
+        this.proficiencies = [];
+        this.expertises = [];
+        this.languages = [];
+
+        
+        this.level = null;
+        this.ac = null;
+        this.initiative = null;
+        this.speed = null;
+        this.inspiration = null;
+        this.hp = null;
+        this.temp_hp = null;
+        this.hit_die_size = null;
+        this.num_of_hit_die = null;
+
+        this.spell_ability = null;
+        this.spell_save_dc = null;
+        this.spell_atk_bonus = null;
+        this.spell_slots = [];
+        this.spell_list = null;
+        this.spells_known = [];
+        this.spells_prepared = [];
+        this.cantrips = [];
+
+        this.age = null;
+        this.height = null;
+        this.weight = null;
+        this.eyes = null;
+        this.skin = null;
+        this.hair = null;
+
+        this.armor = null;
+        this.weapons = [];
+        this.shield = null;
+        this.backpack = null;
+        this.quiver = null;
+        this.tools = [];
+        this.magicitems = [];
+
+        this.currency = [];
+        this.equipment = [];
+        this.custom_equipment = null;
+        this.image_link = null;
+    }
+
+}
