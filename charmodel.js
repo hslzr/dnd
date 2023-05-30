22a87d3b53b2feac5723705f731663cff228d499
DavidAshburn <oneinchwebbing@gmail.com>
2023-05-29 15:32:02 -1000

    acolyte and background class

--- charmodel.js
+++ charmodel.js
@@ -118,9 +118,9 @@ const Dwarven_Race = {
   armor: [],
   tool_choice: [
     1,
-    "Smith's Tools",
-    "Brewer's Supplies",
-    "Mason's Tools",
+    'Smith`s Tools',
+    'Brewer`s Supplies',
+    'Mason`s Tools',
   ],
   size: 'medium',
   speed: 25,
@@ -177,7 +177,7 @@ const Barbarian = {
     [1, ['2 Handaxes', 'any_simple']],
   ],
   equipment: ['4 Javelins'],
-  backpack: ["Explorer's Pack"],
+  backpack: ['Explorer`s Pack'],
 
   features: [
     [
@@ -186,7 +186,7 @@ const Barbarian = {
     ],
     [
       'Reckless Attack: Advantage on your first melee STR attack roll and attacks against you until your next turn.',
-      "Danger Sense: Advantage on DEX saves against effects you can see. You can't do this if you are blinded, deafened, or incapacitated.",
+      'Danger Sense: Advantage on DEX saves against effects you can see. You can`t do this if you are blinded, deafened, or incapacitated.',
     ],
     [
       'Primal Path: Choose a path that shapes the nature of your rage.',
@@ -194,11 +194,11 @@ const Barbarian = {
     ['ASI'],
     [
       'Extra Attack: You may attack twice whenever you take the Attack actin on your turn',
-      "Fast Movement: Your speed increases by 10 feet while you aren't wearing heavy armor",
+      'Fast Movement: Your speed increases by 10 feet while you aren`t wearing heavy armor',
     ],
     [],
     [
-      "Feral Instinct: You have advantage on initiative rolls. If you are surprised at the beginning of combat and aren't incapacitated, you can act normall on your first turn, but only if you enter your rage before doiug anything else.",
+      'Feral Instinct: You have advantage on initiative rolls. If you are surprised at the beginning of combat and aren`t incapacitated, you can act normall on your first turn, but only if you enter your rage before doiug anything else.',
     ],
     ['ASI'],
     [
@@ -206,7 +206,7 @@ const Barbarian = {
     ],
     [],
     [
-      "Relentless Rage: If you drop to 0 hit points while raging and don't die outright, you can make a DC 10 CON save to drop to 1 hit point instead. The DC increases by 5 each time until you finish a short or long rest.",
+      'Relentless Rage: If you drop to 0 hit points while raging and don`t die outright, you can make a DC 10 CON save to drop to 1 hit point instead. The DC increases by 5 each time until you finish a short or long rest.',
     ],
     ['ASI'],
     [
@@ -229,7 +229,7 @@ const Barbarian = {
     ],
   ],
 
-  //two empty at the beginning for levels 1 and 2, I don't want to complicate parsing this data
+  //two empty at the beginning for levels 1 and 2, I don`t want to complicate parsing this data
   //I want to treat it the same as the features array
   subclasses: [
     {
@@ -238,24 +238,24 @@ const Barbarian = {
         [],
         [],
         [
-          'Frenzy: You can go into a frenzy when you rage. If you do so, for the duration of your rage, you can make a single melee weapon attack as a bonus action on each of your turns after this one. When your rage ends, you suffer one level of exhaustion.'
+          'Frenzy: You can go into a frenzy when you rage. If you do so, for the duration of your rage, you can make a single melee weapon attack as a bonus action on each of your turns after this one. When your rage ends, you suffer one level of exhaustion.',
         ],
         [],
         [],
         [
-          'Mindless Rage: You can\'t be charmed or frightened while raging. If you are charmed or frightened when you enter your rage, the effect is suspended for the duration of the rage.'
-        ],
-        [],
-        [],
-        [],
-        [
-          'Intimidating Presence: You can use your action to frighten someone with your menacing presence. Choose one creature you can see within 30 feet, if it can see or hear you it must succeed on a WIS save with DC of 8 plus your proficiency bonus plus your CHA modifier, or be frightened of you until the end of your next turn. On subsequent turns you can use your action to extend this effect another turn. The effect ends if the creature ends its turn out of line of sight or more than 60 feet away. If they successfully save, you can\'t use this on them again for 24 hours.'
+          'Mindless Rage: You can`t be charmed or frightened while raging. If you are charmed or frightened when you enter your rage, the effect is suspended for the duration of the rage.',
         ],
         [],
         [],
         [],
         [
-          'Retaliation: When you take damage from a creature within 5 feet of you, you can use your reaction to make a melee weapon attack against that creature.'
+          'Intimidating Presence: You can use your action to frighten someone with your menacing presence. Choose one creature you can see within 30 feet, if it can see or hear you it must succeed on a WIS save with DC of 8 plus your proficiency bonus plus your CHA modifier, or be frightened of you until the end of your next turn. On subsequent turns you can use your action to extend this effect another turn. The effect ends if the creature ends its turn out of line of sight or more than 60 feet away. If they successfully save, you can`t use this on them again for 24 hours.',
+        ],
+        [],
+        [],
+        [],
+        [
+          'Retaliation: When you take damage from a creature within 5 feet of you, you can use your reaction to make a melee weapon attack against that creature.',
         ],
         [],
         [],
@@ -272,24 +272,24 @@ const Barbarian = {
         [],
         [
           'Spirit Seeker: You gain the ability to cast Beast Sense and Speak With Animals as rituals.',
-          'Totem Spirit: Choose a totem spirit and acquire its feature. You must make or acquire a physical totem object that imcorporates fur or feathers, claws, teeth, or bones of the totem aniumal. At your option, you also gain minior physical attributes that are reminiscent of your totem spirit. For example, if you have a bear totem spirit, you might be unusually hairy and thick-skinned, or if your totem is the eagle, your eyes turn bright yellow. Your totem animal might be an animal related to those listed here but more appropriate to your homeland.'
+          'Totem Spirit: Choose a totem spirit and acquire its feature. You must make or acquire a physical totem object that imcorporates fur or feathers, claws, teeth, or bones of the totem aniumal. At your option, you also gain minior physical attributes that are reminiscent of your totem spirit. For example, if you have a bear totem spirit, you might be unusually hairy and thick-skinned, or if your totem is the eagle, your eyes turn bright yellow. Your totem animal might be an animal related to those listed here but more appropriate to your homeland.',
         ],
         [],
         [],
         [
-          "Aspect of the Beast: You gain a magical benefit based on the totem animal of your choice."
-        ],
-        [],
-        [],
-        [],
-        [
-          "Spirit Walker: You can cast the Commune With Nature spell as a ritual. When you do, a spiritual version of one of your totem animals appears to you to convey the information you seek."
+          'Aspect of the Beast: You gain a magical benefit based on the totem animal of your choice.',
         ],
         [],
         [],
         [],
         [
-          "Totemic Attunement: You gain a magical benefit based on the totem animal of your choice."
+          'Spirit Walker: You can cast the Commune With Nature spell as a ritual. When you do, a spiritual version of one of your totem animals appears to you to convey the information you seek.',
+        ],
+        [],
+        [],
+        [],
+        [
+          'Totemic Attunement: You gain a magical benefit based on the totem animal of your choice.',
         ],
         [],
         [],
@@ -301,20 +301,20 @@ const Barbarian = {
       totem_spirit: null,
       totem_choices: [
         'Bear: While raging, you have resistance to all damage except psychic damage. The spirit of the bear makes you tough enough to stand up to any punishment.',
-        'Eagle: While you\'re ragina and aren\'t wearing heavy armor, other creatures have disadvantage on opportunity attack rolls against you, and you can use the Dash action as a bonus action on your turn. The spirit of the eagle makes you into a predator who can weave through the gray with ease.',
-        "Wolf: While you're raging, your friends have advantage on melee attack rolls against any creature within 5 feet of you that is hostile to you. The spirit of the wolf makes you a leader of hunters."
+        'Eagle: While you`re ragina and aren`t wearing heavy armor, other creatures have disadvantage on opportunity attack rolls against you, and you can use the Dash action as a bonus action on your turn. The spirit of the eagle makes you into a predator who can weave through the gray with ease.',
+        'Wolf: While you`re raging, your friends have advantage on melee attack rolls against any creature within 5 feet of you that is hostile to you. The spirit of the wolf makes you a leader of hunters.',
       ],
       beast_aspect: null,
       aspect_choices: [
-        "Bear: You gain the might of a bear. your carrying capacity is doubled, and you have advantage on STR checks made to push, pull, lift, or break objects.",
-        "Eagle: You gain the eyesight of an eagle. you can see up to 1 mile away with no difficulty, able to discern even fine details as though looking at something no more than 100 feet away from you. Additionally, dim light doesn't impose disadvantage on your Wisdom(Perception) checks.",
-        "Wolf: You gain the hunting sensibilities of a wolf. You can track other creatures while traveling at a fast pace, and you can move stealthily while traveling at a normal pace."
+        'Bear: You gain the might of a bear. your carrying capacity is doubled, and you have advantage on STR checks made to push, pull, lift, or break objects.',
+        'Eagle: You gain the eyesight of an eagle. you can see up to 1 mile away with no difficulty, able to discern even fine details as though looking at something no more than 100 feet away from you. Additionally, dim light doesn`t impose disadvantage on your Wisdom(Perception) checks.',
+        'Wolf: You gain the hunting sensibilities of a wolf. You can track other creatures while traveling at a fast pace, and you can move stealthily while traveling at a normal pace.',
       ],
       totemic_attunement: null,
       attunement_choices: [
-        "Bear: While you're raging, any creature within 5 feet of you that's hostile to you has disadvantage on attack rolls against targets other than you or another character with this feature. An enemy is immune to this effect if it can't see or hear you or if it can't be frightened.",
-        "Eagle: While raging, you have a flying speed equal to your current walking speed. This benefit works only in short bursts; you fall if you end your turn in the air and nothing else is holding you aloft.",
-        "Wolf: While you're raging, you can use a bonus action on your turn to knock a Large or smaller creature prone when you hit it with meleee weapon attack."
+        'Bear: While you`re raging, any creature within 5 feet of you that`s hostile to you has disadvantage on attack rolls against targets other than you or another character with this feature. An enemy is immune to this effect if it can`t see or hear you or if it can`t be frightened.',
+        'Eagle: While raging, you have a flying speed equal to your current walking speed. This benefit works only in short bursts; you fall if you end your turn in the air and nothing else is holding you aloft.',
+        'Wolf: While you`re raging, you can use a bonus action on your turn to knock a Large or smaller creature prone when you hit it with meleee weapon attack.',
       ],
 
       Initial(character, totem_animal) {
@@ -325,16 +325,17 @@ const Barbarian = {
 
       //on level 6 and 14, args must include " aspect_choice/attunement_choice: /*Bear/Eagle/Wolf*/ "
       LevelUp(character, args) {
-        if(character.level == 6) {
-          character.beast_aspect = args.aspect_choice;
+        if (character.level == 6) {
+          character.subclass.beast_aspect = args.aspect_choice;
         }
-        if(character.level == 10) {
+        if (character.level == 10) {
           character.rituals.push('Commune With Nature');
         }
-        if(character.level == 14) {
-          character.subclass.totemic_attunement = args.attunement_choice;
+        if (character.level == 14) {
+          character.subclass.totemic_attunement =
+            args.attunement_choice;
         }
-      }
+      },
     },
   ],
 
@@ -342,10 +343,7 @@ const Barbarian = {
     character.starting_hp += AbilityModifier(character.con);
 
     //Fast Movement
-    if (
-      character.level > 2 &&
-      character.armor_class != 'heavy'
-    ) {
+    if (character.level > 2 && character.armor_class != 'heavy') {
       character.speed += 10;
     }
 
@@ -366,7 +364,6 @@ const Barbarian = {
     if (character.armor_class == 'unarmored') {
       character.ac = 10 + AbilityModifier(character.dex);
     }
-
   },
 
   LevelUp(character) {
@@ -388,3 +385,83 @@ const Barbarian = {
   Subclasses have an Initial() lifecycle function I want called when they are chosen
   as well as a LevelUp() function that should be called to update subclass features
 */
+
+class Background {
+  constructor(background) {
+    this.name = background.name;
+    this.description = background.description;
+
+    this.tools = background.tools;
+    this.armor = background.armor;
+    this.weapons = background.weapons;
+    this.skills = background.skills;
+
+    this.languages = background.languages;
+    this.equipment = background.equipment;
+    this.feature = background.feature;
+    this.traits = background.traits;
+    this.ideals = background.ideals;
+    this.bonds = background.bonds;
+    this.flaws = background.flaws;
+
+    this.specialty = background.specialty;
+    this.functions = background.functions;
+  }
+}
+
+const Acolyte = {
+  name: 'Acolyte',
+  description:
+    'You have spent your life in the service of a temple. You act as an intermediary between the realm of the holy and the mortal world.',
+  tools: [''],
+  armor: [''],
+  weapons: [''],
+  skills: ['insight', 'religion'],
+  languages: 2,
+  equipment: [
+    'A holy symbol',
+    'A prayer book',
+    '5 sticks of incense',
+    'vestments',
+    'common clothes',
+    '15gp',
+  ],
+  traits: [
+    'I idolize a particular hero of my faith, and constantly refer to that person`s deeds and example.',
+    'I can find common ground between the fiercest enemies, empathizing with them and always working toward peace.',
+    'I see omens in every event and action. The gods try to speak to us, we just need to listen.',
+    'Nothing can shake my optimistic attitude.',
+    'I quote (or misquote) sacred texts and proverbs in almost every situation.',
+    'I am tolerant (or intolerant) of other faiths and respect (or condemn) the worship of other gods.',
+    'I’ve enjoyed fine food, drink, and high society among my temple’s elite. Rough living grates on me.',
+    'I’ve spent so long in the temple that I have little practical experience dealing with people in the outside world.',
+  ],
+  ideals: [
+    'Tradition. The ancient traditions of worship and sacrifice must be preserved and upheld. (Lawful)',
+    'Charity. I always try to help those in need, no matter what the personal cost. (Good)',
+    'Change. We must help bring about the changes the gods are constantly working in the world. (Chaotic)',
+    'Power. I hope to one day rise to the top of my faith’s religious hierarchy. (Lawful)',
+    'Faith. I trust that my deity will guide my actions. I have faith that if I work hard, things will go well. (Lawful)',
+    'Aspiration. I seek to prove myself worthy of my god’s favor by matching my actions against his or her teachings. (Any)',
+  ],
+  bonds: [
+    'I would die to recover an ancient relic of my faith that was lost long ago.',
+    'I will someday get revenge on the corrupt temple hierarchy who branded me a heretic.',
+    'I owe my life to the priest who took me in when my parents died.',
+    'Everything I do is for the common people.',
+    'I will do anything to protect the temple where I served.',
+    'I seek to preserve a sacred text that my enemies consider heretical and seek to destroy.',
+  ],
+  flaws: [
+    'I judge others harshly, and myself even more severely.',
+    'I put too much trust in those who wield power within my temple’s hierarchy.',
+    'My piety sometimes leads me to blindly trust those that profess faith in my god.',
+    'I am inflexible in my thinking.',
+    'I am suspicious of strangers and expect the worst of them.',
+    'Once I pick a goal, I become obsessed with it to the detriment of everything else in my life.',
+  ],
+  specialty: [''],
+  functions: [''],
+  feature:
+    'Shelter of the Faithful: You command the respect of those who share your faith, and you can perform the religious ceremonies of your deity. You and your party can expect to receive free healing and care at a temple, shrine, or other established presence of your faith, though you must provide any material components needed for spells. Those who share your religion will support you at a modest lifestyle. You might have ties to a specific temple where you have a residence and the priests can be called upon for non-hazardous assistance near there.',
+};
