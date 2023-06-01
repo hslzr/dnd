304a60f78b3c5f36b792ead7272d59fb30c07fb7
DavidAshburn <oneinchwebbing@gmail.com>
2023-05-31 19:31:48 -0700

    first frame of character sheet laid out

---
+++ app/javascript/controllers/sheet_controller.js
@@ -0,0 +1,46 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    static targets = [
+        "charName",
+        "profBonus",
+        "passPerception",
+        "strBase",
+        "strSavingThrowMod",
+        "athleticsMod",
+        "dexBase",
+        "dexSavingThrowMod",
+        "acrobaticsMod",
+        "sleightOfHandMod",
+        "stealthMod",
+        "conBase",
+        "conSavingThrowMod",
+        "intBase",
+        "intSavingThrowMod",
+        "arcanaMod",
+        "historyMod",
+        "investigationMod",
+        "natureMod",
+        "religionMod",
+        "wisBase",
+        "wisSavingThrowMod",
+        "animalHandlingMod",
+        "insightMod",
+        "medicineMod",
+        "perceptionMod",
+        "survivalMod",
+        "chaBase",
+        "chaSavingThrowMod",
+        "deceptionMod",
+        "intimidationMod",
+        "performanceMod",
+        "persuasionMod",
+
+
+
+
+    ]
+
+}
+
+
