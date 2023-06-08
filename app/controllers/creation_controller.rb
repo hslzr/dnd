a5659a21592e9a6dde01ef40e94c6d77900c2cec
DavidAshburn <oneinchwebbing@gmail.com>
2023-06-08 01:12:20 -1000

    sheet controller hooked up to turbo frame, populates a map with selected values on submission

--- app/controllers/creation_controller.rb
+++ app/controllers/creation_controller.rb
@@ -1,15 +1,26 @@
 class CreationController < ApplicationController
-  before_action :find_races
+  before_action :find_mains
 
   def sheet
     @races = Race.all
+    @player_classes = PlayerClass.all
+    @backgrounds = Background.all
+
     @subraces = @race&.subraces || []
+    @subclasses = @player_class&.subclasses || []
+    @traits = @background&.traits || []
+    @bonds = @background&.bonds || []
+    @ideals = @background&.ideals || []
+    @flaws = @background&.flaws || []
   end
-  
+
   private
 
-    def find_races
+    def find_mains
       @race = Race.find_by(id: params[:race].presence)
       @subrace = Subrace.find_by(id: params[:subrace].presence)
+      @player_class = PlayerClass.find_by(id: params[:player_class].presence)
+      @subclass = Subclass.find_by(id: params[:subclass].presence)
+      @background = Background.find_by(id: params[:background].presence)
     end
 end
--- app/javascript/controllers/sheet_controller.js
+++ app/javascript/controllers/sheet_controller.js
@@ -61,4 +61,28 @@ export default class extends Controller {
     'equipSP',
     'equipCP',
   ];
+
+  connect() {
+    this.choices = new Map();
+    this.choices.set('race', 'none');
+    this.choices.set('subrace', 'none');
+    this.choices.set('player_class', 'none');
+    this.choices.set('subclass', 'none');
+    this.choices.set('background', 'none');
+  }
+
+  update() {
+    let iterchoice = this.choices.entries();
+
+    for (let i = 0; i < 5; i++) {
+      let label = iterchoice.next().value[0];
+      let options = document.getElementById(label).children;
+
+      for (let j = 0; j < options.length; j++) {
+        if (options.item(j).selected)
+          this.choices.set(label, options.item(j).innerText);
+      }
+    }
+    console.log(this.choices);
+  }
 }
