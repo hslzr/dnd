ffe1e212a8ecd61366e5796a250f3834db4e451d
DavidAshburn <oneinchwebbing@gmail.com>
2023-06-26 09:46:11 -1000

    equipment populates to sheet

---
+++ app/controllers/labels_controller.rb
@@ -0,0 +1,15 @@
+class LabelsController < ApplicationController
+  def index
+    @armor = Armor.all.map{|item| item.name}
+    @weapons = Weapon.all.map{|item| item.name}
+    @tools = Tool.all.map{|item| item.name}
+    @equipment = Equipment.all.map{|item| item.name}
+    @gearpacks = Gearpack.all.map{|item| item.name}
+
+    @names = {:weapons => @weapons, :armor => @armor, :tools => @tools, :equipment => @equipment, :gearpacks => @gearpacks }
+
+    respond_to do |format|
+      format.json { render json: @names}
+    end
+  end
+end
